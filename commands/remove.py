ab70825d9c9d0b2b42677fb179b78e5b2eb37954
Daniel Seehausen <Daniel.Seehausen@gmail.com>
2017-07-09 19:40:13 -0400

    Updated delete to show what was deleted

--- commands/remove.py
+++ commands/remove.py
@@ -1,10 +1,14 @@
 def main(args, curs, conn):
-    print(args)
-    print(int(args[0]))
     if not args[0] or not int(args[0]):
-        #if no content was specified
+        # asserting type and presence
         raise ValueError("Primary key required as integer!")
-    query = "DELETE FROM todos WHERE id=" + args[0]
-    curs.execute(query)
-    conn.commit()
-    print("...Removed")
+    row = curs.execute("SELECT * FROM todos WHERE id=?", [args[0]]).fetchall()[0]
+    if row: # sqlite cant select and delete at once i believe (says this most efficient on interwebs)
+        curs.execute("DELETE FROM todos WHERE id=?", [args[0]])
+        conn.commit()
+        print("\nDeleted: ", "|", row[0], "| " + row[2] + ' - ' + row[1])
+    else:
+        print("No matching row...doing nothing")
+
+
+        # print_rows(curs.execute("SELECT * FROM todos WHERE cat=?", [args[0]]))
